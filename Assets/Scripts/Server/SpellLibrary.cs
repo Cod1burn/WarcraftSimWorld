5fc4b6f1f41215560549340351cf52fdba99a156
Yiran Jing <j1burn@outlook.com>
2024-07-09 16:56:57 +0100

    SpellLibrary for all spells

--- Assets/Scripts/Server/SpellLibrary.cs
+++ Assets/Scripts/Server/SpellLibrary.cs
@@ -7,17 +7,28 @@ namespace WorldOfSim.Server
     public class SpellLibrary : MonoBehaviour {
 
         public static SpellLibrary Instance { get; private set; }
-        
+
+        public Dictionary<int, SpellBase<object>> library;
 
 
         void Awake()
         {
             Instance = this;
+            library = new Dictionary<int, SpellBase<object>>();
         }   
 
         void InitializeSpells()
         {
-            // Add spells to the library here
+            // Loop through all files in Assets/Data/SpellData and get all of their file name in string
+            string spellDataPath = "Assets/Data/SpellData";
+            string[] spellFiles = System.IO.Directory.GetFiles(spellDataPath);
+
+            foreach (string spellFile in spellFiles)
+            {
+                string spellFileName = System.IO.Path.GetFileNameWithoutExtension(spellFile);
+                // TODO: string name to class name
+            }
+
         }
     }
     
