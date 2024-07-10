b193c6183a49bc0b980771ce601c618721f49d10
Yiran Jing <j1burn@outlook.com>
2024-07-10 10:37:02 +0100

    Attack Spell

--- Assets/Scripts/CombatComp/Spells/AutoAttack.cs
+++ Assets/Scripts/CombatComp/Spells/AutoAttack.cs
@@ -1,9 +1,18 @@
+using System.Collections.Generic;
 using WorldOfSim.CombatComp.Events;
+using WorldOfSim.CombatUnits;
 
 namespace WorldOfSim.CombatComp.Spells
 {
     public class AutoAttack : SpellBase<AutoAttack>
     {
-
+        public override void OnCast(Unit caster, List<Unit> targets)
+        {
+            foreach (var target in targets)
+            {
+                float RawDamage = Data.Values["AP1"] * caster.ap;
+            }
+            base.OnCast(caster, targets);
+        }
     }
 }
